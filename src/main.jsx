361482d58b75f8701dad7bcefeac6b95fc14e47a
Ranim <55930699+Ran-sy@users.noreply.github.com>
2023-03-16 08:58:07 +0200

    fix url

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from 'https://ran-sy.github.io/ultimate-chat/src/App'
+import 'https://ran-sy.github.io/ultimate-chat/src/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
